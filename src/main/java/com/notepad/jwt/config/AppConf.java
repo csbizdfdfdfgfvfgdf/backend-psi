71c4a2ce68b0fc9f344ce859b4d999f13fa2eb80
NiroshaDilan <dilan.vista@gmail.com>
2021-02-09 23:24:42 +0530

    Uncommented @Configuration

--- src/main/java/com/notepad/jwt/config/AppConf.java
+++ src/main/java/com/notepad/jwt/config/AppConf.java
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class AppConf implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedHeaders("pragma")
                 .allowedOrigins("http://localhost:4202",
                         "http://ppssii.com",
                         "https://apis.ppssii.com",
