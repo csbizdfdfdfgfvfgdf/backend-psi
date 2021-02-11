34d8a6fcb37337d1c5a773e5b1bc805d83e0ca44
Ankush <ankushsinghal1992@gmail.com>
2021-02-11 13:30:30 +0400

    cors fix

--- src/main/java/com/notepad/config/WebSecurityConfig.java
+++ src/main/java/com/notepad/config/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
-		httpSecurity.and().csrf().disable()
+		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
 				.authorizeRequests().antMatchers(
 						"/authenticate", 
