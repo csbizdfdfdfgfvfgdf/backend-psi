b349844930b168241f6d39b93ea084ff32bf376c
SOLITON\Zohaib.Ali <zohaib.ali9242@gmail.com>
2021-02-12 16:03:10 +0500

    Swagger api documentation for specific controllers

--- src/main/java/com/notepad/config/SwaggerConfig.java
+++ src/main/java/com/notepad/config/SwaggerConfig.java
@@ -24,7 +24,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
         	.select()
-            .apis(RequestHandlerSelectors.any())
+            .apis(RequestHandlerSelectors.basePackage("com.notepad.controller"))
             .paths(PathSelectors.any())
             .build()
             .apiInfo(apiInfo());
