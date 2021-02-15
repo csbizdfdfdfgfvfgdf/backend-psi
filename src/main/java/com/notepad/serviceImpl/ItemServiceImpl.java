869869654b2bc03922a0a64dd5cdaa0918346962
SOLITON\Zohaib.Ali <zohaib.ali9242@gmail.com>
2021-02-15 17:14:31 +0500

    save and update item API updated - better performance

--- src/main/java/com/notepad/serviceImpl/ItemServiceImpl.java
+++ src/main/java/com/notepad/serviceImpl/ItemServiceImpl.java
@@ -50,7 +50,11 @@ public class ItemServiceImpl implements ItemService {
 	public List<ItemDTO> save(List<ItemDTO> itemDTOs, Principal principal) {
 		log.info("Request to create item : {} ", itemDTOs);
 
+		// newItems to return saved items
 		List<ItemDTO> newItems = new ArrayList<ItemDTO>();
+		
+		// itemToUpdateList to update list of items
+		List<Item> itemToUpdateList = new ArrayList<Item>();
 		
 		for (ItemDTO itemDTO: itemDTOs) {
 			
@@ -72,8 +76,11 @@ public class ItemServiceImpl implements ItemService {
 				if(newItem.getMenu().getMenuId()==null){
 					newItem.setMenu(null);
 				}
-				Item item = itemRepository.save(newItem);
-				newItems.add(itemMapper.toDTO(item));
+				
+				itemToUpdateList.add(newItem);
+				
+//				Item item = itemRepository.save(newItem);
+//				newItems.add(itemMapper.toDTO(item));
 				
 			} else {
 				// to update item
@@ -99,226 +106,18 @@ public class ItemServiceImpl implements ItemService {
 					existingItem.setOrderId(itemDTO.getOrderId());
 				});
 				
-				Item updatedItem = itemRepository.save(itemToUpdate.get());
-				newItems.add(itemMapper.toDTO(updatedItem));
+				itemToUpdateList.add(itemToUpdate.get());
 			}
+		}
+
+		List<Item> updatedItems = itemRepository.saveAll(itemToUpdateList);
+		
+		// convert saved items to itemDTOs
+		for(Item updatedItem: updatedItems) {
+			newItems.add(itemMapper.toDTO(updatedItem));
 		}
 		
 		return newItems;
-		
-		// to create item
-
-		/*
-		
-		if (itemDTO.getItemId() == null) {
-			User user = userRepository.findByUserName(principal.getName());
-			if (user != null) {
-				// set user to item
-				itemDTO.setuId(user.getUserId());
-				itemDTO.setUserName(user.getUserName());
-			}
-
-			if (itemDTO.getpId() == null && itemDTO.getContent() == null) {
-				throw new BadRequestAlertException("Content and parentId must not be null", "Item", null);
-			}
-
-			// set order
-
-			if (itemDTO.getOrderId() != null) { // check if order presents in requestBody
-				// if yes, then do relevant changes
-
-				List<Item> items = new ArrayList<>();
-
-				// 1) get Items by the pId of item to be stored --> sortedByOrder
-
-				Long pId = itemDTO.getpId();
-
-				// get all items with pId
-				Optional<Menu> parentMenu = menuRepository.findById(pId);
-				if (parentMenu != null) {
-					items = itemRepository.findAllByMenuOrderByOrderId(parentMenu.get());
-				}
-
-				if (!items.isEmpty()) {
-
-					for (Item existingItem : items) {
-						if (existingItem.getOrderId() < itemDTO.getOrderId()) {
-							// 1.2) if Items (sortedByOrder) exist, then iterate until items'
-							// existingItemOder < newItemOrder
-							continue;
-						} else {
-							// 1.2.1) then make oder+1 for remaining Items
-							existingItem.setOrderId(existingItem.getOrderId() + 1);
-						}
-					}
-
-					// 1.2.2) setNewItemOder as per requestBody
-					itemDTO.setOrderId(itemDTO.getOrderId());
-
-				}
-
-			} else { // if not then to assign last order --> follow the steps below!
-
-				List<Item> items = new ArrayList<>();
-
-				// 1) get Items by the pId of item to be stored
-
-				Long pId = itemDTO.getpId();
-
-				// get all items with pId
-				if(pId != null){
-					Optional<Menu> parentMenu = menuRepository.findById(pId);
-					if (parentMenu != null) {
-						items = itemRepository.findAllByMenuOrderByOrderId(parentMenu.get());
-					}
-				}
-				if (items.isEmpty()) {
-					// 1.1) if Items do not exist, then set the first order as 0
-					itemDTO.setOrderId(0);
-				} else {
-					// 1.2) if exists, then get item with maxOrder
-					Item lastIndexedItem = items.get(items.size() - 1);
-
-					// 1.2.1) set maxOrder+1 to the order of item to be stored
-					itemDTO.setOrderId(lastIndexedItem.getOrderId() + 1);
-				}
-
-			}
-			Item newItem = itemMapper.toEntity(itemDTO);
-			if(newItem.getMenu().getMenuId()==null){
-				newItem.setMenu(null);
-			}
-			Item item = itemRepository.save(newItem);
-			return itemMapper.toDTO(item);
-
-		} else { // to update item
-
-			Optional<Item> item = itemRepository.findById(itemDTO.getItemId());
-			item.ifPresent(existingItem -> {
-				if (itemDTO.getContent() != null) {
-					existingItem.setContent(itemDTO.getContent());
-				}
-
-				// TODO : to update pId of item
-				
-				if (existingItem.getMenu().getMenuId() != itemDTO.getpId()) {
-					
-					
-					// update z-indices in item's old parent Menu!
-					
-//					List<Item> oldItems = new ArrayList<>();
-//					
-//					Long oldPId = existingItem.getMenu().getMenuId();
-//					
-//					// get all items with old-pId
-//					Optional<Menu> oldParentMenu = menuRepository.findById(oldPId);
-//					if (oldParentMenu != null) {
-//						oldItems = itemRepository.findAllByMenuOrderByOrder(oldParentMenu.get());
-//					}
-//					if (oldItems.size() > 1) {
-//						for (Item existingItem1 : oldItems) {
-//							if (existingItem1.getItemId() == existingItem.getItemId()
-//									&& existingItem1.getItemId().equals(existingItem.getItemId())) {
-//								continue;
-//							}
-//							if (existingItem1.getOrder() < existingItem.getOrder()) {
-//								continue;
-//							} else {
-//								existingItem1.setOrder(existingItem1.getOrder() - 1);
-//							}
-//						}
-//					}
-					
-					
-					
-					// get MenuBy menuDTO.getpId()
-					
-					Optional<Menu> menuOp = menuRepository.findById(itemDTO.getpId());
-					if (menuOp.isPresent()) {
-						// set this menu to existing item
-						existingItem.setMenu(menuOp.get());
-					}
-					
-					
-					// set last index as z-index to this item
-					
-					List<Item> items = new ArrayList<>();
-
-					// 1) get Items by the pId of item to be stored
-
-					Long pId = itemDTO.getpId();
-
-					// get all items with pId
-					Optional<Menu> parentMenu = menuRepository.findById(pId);
-					if (parentMenu != null) {
-						items = itemRepository.findAllByMenuOrderByOrderId(parentMenu.get());
-					}
-
-					if (items.isEmpty()) {
-						// 1.1) if Items do not exist, then set the first order as 0
-						existingItem.setOrderId(0);
-					} else {
-						// 1.2) if exists, then get item with maxOrder
-						Item lastIndexedItem = items.get(items.size() - 1);
-
-						// 1.2.1) set maxOrder+1 to the order of item to be stored
-						existingItem.setOrderId(lastIndexedItem.getOrderId() + 1);
-					}
-					
-				}
-				
-				// to update z-index
-
-				if (itemDTO.getOrderId() != null) { // check if order presents in requestBody
-					// if yes, then do relevant changes
-
-					List<Item> items = new ArrayList<>();
-
-					// 1) get Items by the pId of item to be stored --> sortedByOrder
-
-					Long pId = itemDTO.getpId();
-
-					// get all items with pId
-					Optional<Menu> parentMenu = menuRepository.findById(pId);
-					if (parentMenu != null) {
-						items = itemRepository.findAllByMenuOrderByOrderId(parentMenu.get());
-					}
-
-					if (items.isEmpty()) {
-						// 1.1) if items do not exist, then set the first orderId as per requestBody
-						existingItem.setOrderId(itemDTO.getOrderId());
-					}
-					if (!items.isEmpty()) {
-
-						for (Item existingItem1 : items) {
-							if (existingItem1.getItemId().equals(itemDTO.getItemId())
-									&& existingItem1.getItemId() == itemDTO.getItemId()) {
-								// skip for item itself
-								continue;
-							}
-							if (existingItem1.getOrderId() < itemDTO.getOrderId()) {
-								// 1.2) if Items (sortedByOrder) exist, then iterate until items'
-								// existingItemOrder < newItemOrder
-								continue;
-							} else {
-								// 1.2.1) then make order+1 for remaining Items
-								existingItem1.setOrderId(existingItem1.getOrderId() + 1);
-							}
-						}
-
-						// 1.2.2) setNewItemOrder as per requestBody
-						existingItem.setOrderId(itemDTO.getOrderId());
-					}
-				}
-
-			});
-
-			Item updatedItem = itemRepository.save(item.get());
-			return itemMapper.toDTO(updatedItem);
-		}
-		
-		*/
-
 	}
 
 	/**
