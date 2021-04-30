04beb6559790aabc94e190b2fb9da27753e508a9
cscsb <49179157+cscsb@users.noreply.github.com>
2021-04-30 20:12:52 +0800

    fix

--- src/main/java/com/notepad/repository/ItemRepository.java
+++ src/main/java/com/notepad/repository/ItemRepository.java
@@ -21,22 +21,15 @@ import com.notepad.entity.Menu;
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
 	List<Item> findAllByMenu(Menu menu);
-<<<<<<< Updated upstream
-	
+
 	List<Item> findAllByMenuOrderByOrderId(Menu menu);
 	
 	List<Item> findAllByUserAndMenu(User user,Menu menu);
-=======
-
-
 
 	List<Item> findAllByMenuOrderByOrderId(Menu menu);
-
-
-
+	
 	List<Item> findAllByUserAndMenu(User user,Menu menu);
 
 
 
->>>>>>> Stashed changes
-}
+
